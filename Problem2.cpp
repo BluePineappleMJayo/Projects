1481fb36db0d112dd31ac0e61c73cd8f0b2ba34e
BluePineappleMJayo <48307337+BluePineappleMJayo@users.noreply.github.com>
2019-03-06 23:18:38 -0600

    Add files via upload

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,98 +1,39 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
-
 using namespace std;
 
-int getCompSelection(int compH, string& comH);
-int getUserSelection(int userH, string& useH);
-int compare(int userH, int compH);
-string hand(int);
-void playResults(int &winner, int& tie, int& compW, int& userW, string useH, string comH);
-void finalResults(int tie, int compW, int userW);
+int findingFibN(int);
 
 int main()
 {
-	int compH, userH, tie=0, compW=0, userW=0, winner;
-	string comH, useH;
-	char ans='y';
-
+	int n,fibN;
+	char again = 'y';
+	do{
 	do
 	{
-		cout<< "It's time to play ROCK, PAPER, SCISSORS!\n";
-		userH = getUserSelection(userH,useH);
-		compH = getCompSelection(compH,comH);
-		winner = compare(userH,compH);
-		playResults(winner,tie, compW, userW, useH, comH);
-		cout << "Play again? (Y/N): ";
-		cin >> ans;
-	}while (ans == 'Y' || ans == 'y');
-	finalResults(tie, userW, compW);
-	return 0;
+		cout << "Enter a positive integer n: ";
+		cin >> n;
+	}while (n<=0);
+	fibN= findingFibN(n);
+	cout << n << "th Fibonacci number is: " << fibN
+		<< "\n Continue?(Y/N): ";
+		cin >> again;
+	}while(again == 'y' || again == 'Y');
 }
-int getCompSelection(int compH, string& comH)
-{	
-	srand(time(NULL));
-	compH = rand () % 3+1;
-	comH = hand(compH);
-	return compH;
-}
-int getUserSelection(int userH, string& useH)
+int findingFibN(int n)
 {
-	cout << "What's your play? \n (1) Rock (2) Paper (3) Scissors : ";
-	cin >> userH;
-	useH = hand(userH);
-	return userH;
-}
-int compare(int userH, int compH)
-{
-	if ((userH == 1 && compH == 2) || (userH == 2 && compH == 3) || (userH == 3 && compH == 1) )
+	int temp1=0,temp2=1,next=0;
+	if (n == 1)
+		return temp1;
+	if (n== 2)
+		return temp2;
+	int i;
 	{
-		return 1;
+		for(i=2; i<n; i++)
+		{
+			next = temp1+temp2;
+			temp1=temp2;
+			temp2=next;
+		}
 	}
-	else if(userH == compH)
-	{
-		return 0;
-	}
-	else
-	{
-		return 2;
-	}
-}
-void playResults(int &winner, int& tie, int& compW, int& userW, string useH, string comH)
-{
-	cout << "You played " << useH << " and I played " << comH;
-	switch (winner)
-	{
-		case 1:
-			compW ++;
-			cout << "\nI win.";
-			break;
-		case 0:
-			tie ++;
-			cout << "\nThe play was a tie.";
-			break;
-		case 2:
-			userW ++;
-			cout << "\nYou win.";
-			break;
-	}
-}
-void finalResults(int tie, int userW, int compW)
-{
-	cout << "Final results of our match: "
-		<< "\nYou won " << userW << " time(s)."
-		<< "\nI won " << compW << " time(s). "
-		<<"\nThere were " << tie << " tie(s).";
-	cout << "\nBye, bye . . . ";
-}
-string hand(int userH)
-{
-	if (userH == 1)
-		return "rock";
-	else if (userH == 2)
-		return "paper";
-	else
-		return "scissors";
+	return next;
 }
